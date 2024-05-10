4da7e102457b35f41eb190d23f06e928ae668b11
Boubakar PI <agenceweb1919@gmail.com>
2024-05-10 18:41:10 +0000

    10 mai 2024, 18:41

--- exoapplication.sql
+++ exoapplication.sql
@@ -146,4 +146,7 @@ LIMIT 1;
 
 SELECT P.numSS AS NuméroPatient, C.numRPPS AS NuméroMedecin
 FROM CONSULTE AS C JOIN PATIENT P ON (C.numSS = P.numSS)
-WHERE C.numRPPS != P.numSS;
+WHERE C.numRPPS != P.numSS
+GROUP BY C.numSS;
+
+-- 15
